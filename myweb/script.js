66c86e778bede644bb56cc889fca8aa6729a156c
ssci2021 <71858444+ssci2021@users.noreply.github.com>
2020-10-03 04:20:38 +0800

    Add files via upload

---
+++ myweb/script.js
@@ -0,0 +1,29 @@
+		
+		
+		
+		
+		
+		
+		function f_calls_visible() {
+			document.getElementById("id_callsForPapers").style.display="block";
+			document.getElementById("id_callsForSpecialSessions").style.display="block";
+			document.getElementById("id_callsForTutorials").style.display="block";
+		}
+
+		function f_calls_invisible() {
+		  document.getElementById("id_callsForPapers").style.display="none";
+		  document.getElementById("id_callsForSpecialSessions").style.display="none";
+		  document.getElementById("id_callsForTutorials").style.display="none";
+		}
+		
+		function f_program_visible() {
+			document.getElementById("id_womenInComputationalIntelligence").style.display="block";
+			document.getElementById("id_studentActivitiess").style.display="block";
+			document.getElementById("id_publicLecture").style.display="block";
+		}
+
+		function f_program_invisible() {
+		  document.getElementById("id_womenInComputationalIntelligence").style.display="none";
+		  document.getElementById("id_studentActivitiess").style.display="none";
+		  document.getElementById("id_publicLecture").style.display="none";
+		}
