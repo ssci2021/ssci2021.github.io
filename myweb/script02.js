70c685b7f2eecc80043f602108f033a17573ce9b
ssci2021 <71858444+ssci2021@users.noreply.github.com>
2020-10-10 04:49:20 +0800

    Add files via upload

--- myweb/script02.js
+++ myweb/script02.js
@@ -6,72 +6,107 @@
 		
 		function f_calls_visible() {
 			document.getElementById("id_calls_div").style.display="block";
+			document.getElementById("id_calls_div").style.marginLeft = "calc(103.4%)";
+			document.getElementById("id_calls_div").style.marginTop = "calc(-216.4%)";
 			document.getElementById("id_calls_li").style.backgroundColor="#000000";
+			document.getElementById("id_calls_li").style.color="#ffffff";
+			document.getElementById("id_calls_li").style.marginLeft="8px";
+			document.getElementById("id_calls_li").style.paddingLeft="18px";
+			document.getElementById("id_calls_li").style.width="calc(100% - 10px)";
 		}
 
 		function f_calls_invisible() {
 			document.getElementById("id_calls_div").style.display="none";
 			document.getElementById("id_calls_li").style.backgroundColor="#ffffff";
+			document.getElementById("id_calls_li").style.color="#00008b";
+			document.getElementById("id_calls_li").style.marginLeft="-6px";
+			document.getElementById("id_calls_li").style.paddingLeft="18px";
 		}
 		
 		function f_program_visible() {
 			document.getElementById("id_program_div").style.display="block";
+			document.getElementById("id_program_div").style.marginLeft = "calc(103.4%)";
+			document.getElementById("id_program_div").style.marginTop = "calc(-131.6%)";
 			document.getElementById("id_program_li").style.backgroundColor="#000000";
+			document.getElementById("id_program_li").style.color="#ffffff";
+			document.getElementById("id_program_li").style.marginLeft="8px";
+			document.getElementById("id_program_li").style.paddingLeft="18px";
+			document.getElementById("id_program_li").style.width="calc(100% - 10px)";
 		}
 
 		function f_program_invisible() {
 			document.getElementById("id_program_div").style.display="none";
 			document.getElementById("id_program_li").style.backgroundColor="#ffffff";
+			document.getElementById("id_program_li").style.color="#00008b";
+			document.getElementById("id_program_li").style.marginLeft="-6px";
+			document.getElementById("id_program_li").style.paddingLeft="18px";
+		}
+		
+		
+		
+		function func_menu_onmouseover(x) {
+			x.style.backgroundColor="#000000";
+			x.style.marginLeft="8px";
+			x.style.paddingLeft="18px";
+			x.style.color="#ffffff";
+			x.style.width="calc(100% - 10px)";
+		}
+		function func_menu_onmouseout(x) {
+			x.style.backgroundColor="#ffffff";
+			x.style.marginLeft="-6px";
+			x.style.paddingLeft="18px";
+			x.style.color="#00008b";
+		}
+		
+		function func_select_onmouseover(x) {
+			x.style.backgroundColor="#0000ff";
+			x.style.marginLeft="6px";
+			x.style.paddingLeft="18px";
+			x.style.color="#ffffff";
+			x.style.width="calc(100% - 10px)";
+		}
+		function func_select_onmouseout(x) {
+			x.style.backgroundColor="#cccccc";
+			x.style.marginLeft="-6px";
+			x.style.paddingLeft="18px";
+			x.style.color="#00008b";
 		}
 		
 		
 		
 		
-		
-		
-		function func_ADPRL_onclick() {
-			if(document.getElementById("id_ADPRL_a").style.display == "block"){
-				document.getElementById("id_ADPRL_a").style.display="none";
-			}
-			else{
-				document.getElementById("id_ADPRL_a").style.display="block";
-			}
-		}
-		function func_CIBCI_onclick() {
-			if(document.getElementById("id_CIBCI_a").style.display == "block"){
-				document.getElementById("id_CIBCI_a").style.display="none";
-			}
-			else{
-				document.getElementById("id_CIBCI_a").style.display="block";
-			}
-		}
+
 		function func_symposia_onclick(x) {
 			if(x.nextElementSibling.style.display == "block"){
 				x.nextElementSibling.style.display="none";
 				x.style.backgroundColor="#000000";
+				x.style.color="#ffffff";
 			}
 			else{
 				x.nextElementSibling.style.display="block";
 				x.style.backgroundColor="#0000ff";
-				
+				x.style.color="#ffffff";
 			}
 		}
 		function func_symposia_onmouseover(x) {
 			if(x.nextElementSibling.style.display == "block"){
 				x.style.backgroundColor="#000000";
-				
+				x.style.color="#ffffff";
 			}
 			else{
 				x.style.backgroundColor="#000000";
 				x.style.marginLeft="-18px";
+				x.style.color="#ffffff";
 			}
 		}
 		function func_symposia_onmouseout(x) {
 			if(x.nextElementSibling.style.display == "block"){
 				x.style.backgroundColor="#0000ff";
+				x.style.color="#ffffff";
 			}
 			else{
 				x.style.backgroundColor="#ffffff";
 				x.style.marginLeft="-6px";
+				x.style.color="#00008b";
 			}
 		}
